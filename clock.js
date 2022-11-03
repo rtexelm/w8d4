867395385c9d2f1a1c69cd290164f3511a426500
IshsGit <aishtahad@yahoo.com>
2022-11-03 12:16:22 -0400

    building constructor

---
+++ clock.js
@@ -0,0 +1,24 @@
+class Clock {
+    constructor() {
+      // 1. Create a Date object.
+      let date = new Date();
+      // 2. Store the hours, minutes, and seconds.
+      let hours = date.getHours();
+      let minutes = date.getSeconds();
+      let seconds = date.getMinutes();
+      // 3. Call printTime.
+      // 4. Schedule the tick at 1 second intervals.
+    }
+  
+    printTime() {
+      // Format the time in HH:MM:SS
+      // Use console.log to print it.
+    }
+  
+    _tick() {
+      // 1. Increment the time by one second.
+      // 2. Call printTime.
+    }
+  }
+  
+  const clock = new Clock();
