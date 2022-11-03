5d37e211e897c3129e138e5da99098d607603d74
IshsGit <aishtahad@yahoo.com>
2022-11-03 14:39:27 -0400

    made fancy clock

--- clock.js
+++ clock.js
@@ -3,22 +3,30 @@ class Clock {
       // 1. Create a Date object.
       let date = new Date();
       // 2. Store the hours, minutes, and seconds.
-      let hours = date.getHours();
-      let minutes = date.getSeconds();
-      let seconds = date.getMinutes();
+      this.hours = date.getHours();
+      this.minutes = date.getMinutes();
+      this.seconds = date.getSeconds();
       // 3. Call printTime.
+      this.printTime();
       // 4. Schedule the tick at 1 second intervals.
-    }
+      setInterval(this._tick,1000);
+    };
   
     printTime() {
       // Format the time in HH:MM:SS
+      const formatTime = `${this.hours}:${this.minutes}:${this.seconds}`
+      // console.log(`${this.hours}:${this.minutes}:${this.seconds}`)
       // Use console.log to print it.
+      console.log(formatTime);
     }
   
     _tick() {
       // 1. Increment the time by one second.
+      clock.seconds++;
       // 2. Call printTime.
+      clock.printTime();
     }
   }
   
   const clock = new Clock();
+  
