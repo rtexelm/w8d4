616c4eabc5699686cb8c16c5a28db6d7a9bc85f6
rtexelm <texelist@gmail.com>
2022-11-03 15:27:51 -0400

    Debugging addNumbers

---
+++ addnumbers.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function  addNumbers (sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        rl.question("Give me a num", (response) => {
+            parseInt(response) + sum;
+            console.log(newSum);
+            rl.close();
+        }
+        // rl.close()
+        )
+        debugger;
+    } else {
+        return addNumbers (sum, numsLeft - 1, completionCallback)
+    }
+
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
